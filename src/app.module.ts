f65f1f8eb8d30a3ff306adecad9df6a9c96873f2
Andrey <andreikrauchanka1994@gmail.com>
2025-03-15 17:59:28 +0300

    feat: config pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+import { getPostgresConfig } from "./configs/postgres.config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `src/envs/.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync(getPostgresConfig())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/postgres.config.ts
@@ -0,0 +1,19 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const getPostgresConfig = (): TypeOrmModuleAsyncOptions => {
+  return {
+    inject: [ConfigService],
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('SQL_HOST'),
+      port: +configService.get('SQL_PORT'),
+      username: configService.get('SQL_USER_NAME'),
+      password: configService.get('SQL_PASSWORD'),
+      database: configService.get('SQL_NAME'),
+      entities: [],
+      synchronize: true,
+    }),
+  };
+};
